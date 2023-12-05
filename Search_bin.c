42874b62391400c16ba25ca08bb8251f36960d3a
楚子航 <134595926+Guo-Hai-feng@users.noreply.github.com>
2023-12-05 11:34:28 +0800

    折半查找：递归和非递归算法

--- Search_bin.c
+++ Search_bin.c
@@ -1,5 +1,5 @@
 #include"search.h"
-int Search_Bin(SSTable ST, KeyType key)
+int Search_Bin1(SSTable ST, KeyType key)
 {
 	int low = 1, high = ST.length;//ÖÃÇø¼ä´¦ÖÃ
 	while (low <= high)
@@ -13,3 +13,17 @@ int Search_Bin(SSTable ST, KeyType key)
 	}
 	return 0;
 }
+
+int Search_Bin2(SSTable ST, KeyType key,int low,int high)
+{
+	if (low > high)
+		return 0;
+	int mid = (low + high) / 2;
+	if (key == ST.R[mid].key)
+		return mid;
+	else if (key < ST.R[mid].key)
+		Search_Bin(ST, key, low, mid - 1);
+	else
+		Search_Bin(ST, key, mid + 1, high);
+}
+
